e8d53ea2ceb841d0a51572be33cfcfee227a1307
t01604 <pedro.maschio@compasso.com.br>
2025-03-13 18:11:05 -0300

    feat: add not mandatory query params

--- 03-query-parameters/main.py
+++ 03-query-parameters/main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/products')
-def products(id, price):
+def products(id, price): # This way the query parameters are mandatory
+    return {'product': id, 'price': price}
+
+@app.get('/products/v2')
+def products(id: int = 1, price: int = 0): # This way the query parameters are not mandatory and have a default value
     return {'product': id, 'price': price}
