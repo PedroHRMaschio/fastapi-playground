4adf1fd593bb2d6345f7864ce4b7002ac215daee
t01604 <pedro.maschio@compasso.com.br>
2025-03-13 15:05:28 -0300

    feat: add new routes users and profiles to learn about parameters

--- simple-api/main.py
+++ simple-api/main.py
@@ -2,9 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 def index():
     return 'Hello world!'
+
+
+# id here is an id parameter
+@app.get('/users/{id}')
+def users(id: int):
+    return f'This is a user page for user {id}'
+
+
+@app.get('/profiles/{username}')
+def profile(username: str):
+    return {f'This is a profile page for user : {username}'}
+
 
 @app.get('/movies')
 def movies():
