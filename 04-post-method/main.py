212bd4005f829471ee32961256ddc270173fbfee
t01604 <pedro.maschio@compasso.com.br>
2025-03-13 18:38:00 -0300

    feat: new example routes on post method

--- 04-post-method/main.py
+++ 04-post-method/main.py
@@ -6,8 +6,24 @@ class User(BaseModel): # This is the request body for POST /users
     email: str
     age: int
 
+class Product(BaseModel):
+    name: str
+    price: int
+    discount: int
+    discounted_price: float = 0
+
 app = FastAPI()
 
 @app.post('/users')
 def create_user(user:User):
     return {'user data': user}
+
+@app.post('/products')
+def create_product(product: Product):
+    product.discounted_price = product.price - \
+        (product.price * product.discount)/100
+    return product
+
+@app.post('/purchase')
+def purchase(user: User, product: Product):
+    return {'user': user, 'product': product}
