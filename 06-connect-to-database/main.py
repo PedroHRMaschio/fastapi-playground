0cae6810f8e000f7cafac05ae0e284e361b05676
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2025-03-14 15:09:09 -0300

    feat: add status codes to our requests

--- 06-connect-to-database/main.py
+++ 06-connect-to-database/main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
-from sqlalchemy.sql.functions import mode
 import schemas
 import models
 from database import engine, SessionLocal
@@ -17,9 +16,7 @@ def get_db():
     finally:
         db.close()
 
-
-
-@app.post('/product')
+@app.post('/product', status_code=status.HTTP_201_CREATED)
 def create_product(request: schemas.Product, db: Session = Depends(get_db)):
     new_product = models.Product(name=request.name, description=request.description, price=request.price)
     db.add(new_product)
@@ -31,7 +28,7 @@ def create_product(request: schemas.Product, db: Session = Depends(get_db)):
 def update_product(id: int, request: schemas.Product, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id)
     if not product.first():
-        return 'Product not found'
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Product not found')
     product.update(request.model_dump())
     db.commit()
     return 'updated'
@@ -44,6 +41,8 @@ def get_all_products(db: Session = Depends(get_db)):
 @app.get('/product/{id}', response_model=schemas.DisplayProduct)
 def get_product(id: int, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id).first()
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Product not found')
     return product
 
 @app.delete('/product/{id}')
