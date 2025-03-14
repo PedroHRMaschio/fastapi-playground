a4f76ceb5f35a60a201e5e68fbc0ebe5ebd7919e
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2025-03-14 10:01:02 -0300

    feat: add feature that inserts a products into the db

--- 06-connect-to-database/main.py
+++ 06-connect-to-database/main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
-from .import schemas
-from .import models
-from .database import engine
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import mode
+import schemas
+import models
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
 @app.post('/product')
-def create_product(request: schemas.Product):
-    return request
+def create_product(request: schemas.Product, db: Session = Depends(get_db)):
+    new_product = models.Product(name=request.name, description=request.description, price=request.price)
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
