b45264f67ade861545afc84830a0f71fb2b8e49f
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2025-03-16 22:29:00 -0300

    feat: improve routers

--- 06-connect-to-database/routers/product.py
+++ 06-connect-to-database/routers/product.py
@@ -5,9 +5,12 @@ import schemas
 import models
 from database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    tags=['Product'],
+    prefix='/product'
+)
 
-@router.post('/product', status_code=status.HTTP_201_CREATED, tags=['product'])
+@router.post('/', status_code=status.HTTP_201_CREATED)
 def create_product(request: schemas.Product, db: Session = Depends(get_db)):
     new_product = models.Product(name=request.name, description=request.description, price=request.price, seller_id=request.seller_id)
     db.add(new_product)
@@ -15,7 +18,7 @@ def create_product(request: schemas.Product, db: Session = Depends(get_db)):
     db.refresh(new_product)
     return new_product
 
-@router.put('/product/{id}', tags=['product'])
+@router.put('/{id}')
 def update_product(id: int, request: schemas.Product, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id)
     if not product.first():
@@ -24,19 +27,19 @@ def update_product(id: int, request: schemas.Product, db: Session = Depends(get_
     db.commit()
     return 'updated'
 
-@router.get('/product', response_model=list[schemas.DisplayProduct], tags=['product'])
+@router.get('/', response_model=list[schemas.DisplayProduct])
 def get_all_products(db: Session = Depends(get_db)):
     products = db.query(models.Product).all()
     return products
 
-@router.get('/product/{id}', response_model=schemas.DisplayProduct, tags=['product'])
+@router.get('/{id}', response_model=schemas.DisplayProduct)
 def get_product(id: int, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id).first()
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Product not found')
     return product
 
-@router.delete('/product/{id}', tags=['product'])
+@router.delete('/{id}')
 def delete_product(id: int, db: Session = Depends(get_db)):
     db.query(models.Product).filter(models.Product.id == id).delete(synchronize_session=False)
     db.commit()
--- 06-connect-to-database/routers/seller.py
+++ 06-connect-to-database/routers/seller.py
@@ -6,11 +6,14 @@ import models
 from database import get_db
 from passlib.context import CryptContext
 
-router = APIRouter()
+router = APIRouter(
+    tags=['Seller'],
+    prefix='/seller'
+)
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-@router.post('/seller', response_model=schemas.DisplaySeller, status_code=status.HTTP_201_CREATED, tags=['seller'])
+@router.post('/', response_model=schemas.DisplaySeller, status_code=status.HTTP_201_CREATED)
 def create_seller(request: schemas.Seller, db: Session = Depends(get_db)):
     hashed_password = pwd_context.hash(request.password)
     new_seller = models.Seller(username=request.username, email=request.email, password=hashed_password)
